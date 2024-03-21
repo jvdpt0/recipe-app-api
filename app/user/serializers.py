e2e2eb59381774b4cd70889bf2be078b5a1ec720
jvdpt0 <92344705+jvdpt0@users.noreply.github.com>
2024-03-21 20:45:38 -0300

    :sparkles: Adição da funcionalidade e testes para criação de receitas usando a API.
    Adição também de testes para usuários autenticados (privados).
    Correção de escrita em algumas áreas.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,8 +22,19 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return a user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
 
-class AuthTokenSerialzier(serializers.Serializer):
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
     """Serializer for user auth token."""
     email = serializers.EmailField()
     password = serializers.CharField(
